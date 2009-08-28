66299b61c302cb4b5f34f6ab7810224ff1877f70
mpclocal <sammy.zahabi@gmail.com>
2009-08-27 19:20:03 -0700

    save and load ui for previous wsdls

--- app.rb
+++ app.rb
@@ -7,20 +7,49 @@ gem 'soap4r'
 
 require File.dirname(File.expand_path(__FILE__))+'/lib/sinatra_app_helpers'
 
+def update_saved_forms
+  @saved_requests = Dir.entries(Dir.pwd + "/public/saved_forms/requests/").grep(/\.xml$/)
+  @saved_responses = Dir.entries(Dir.pwd + "/public/saved_forms/responses/").grep(/\.xml$/)
+  @saved_wsdls = []
+  @saved_wsdls = File.open(Dir.pwd + "/public/saved_forms/saved_wsdls.txt").readlines
+  @saved_wsdls.map!{|e| e.chomp}
+  @saved_wsdls -= [""] #remove blank lines
+end
+
+def save_wsdls
+  f = File.open(Dir.pwd + "/public/saved_forms/saved_wsdls.txt", "w+")
+  @saved_wsdls.each do |wsdl|
+    f.syswrite(wsdl.to_s + "\n")
+  end  
+end
+
 get '/**' do
   #start page to select the wsdl
+  update_saved_forms
   haml :choose_wsdl
 end
 
 post '/generate' do
+
   @curr = nil
   @namespace = nil
   @folder_name = nil
   @driver_file = nil
   @client = nil
-  if @params[:action] == 'Upload'
+  if @params[:action] == 'LoadWsdl'
+    #don't do anything special
+  elsif @params[:action] == 'DeleteWsdl'
+    update_saved_forms
+    @saved_wsdls -= [@params[:wsdl].to_s]
+    save_wsdls
+    return haml :choose_wsdl
+  elsif @params[:action] == 'Upload'
     File.open("public/saved_forms/requests/#{@params['datafile'][:filename]}", "w+").syswrite(@params["datafile"][:tempfile].readlines)
     @params[:wsdl] = Dir.pwd.to_s + "/public/saved_forms/requests/#{@params['datafile'][:filename]}"
+  elsif @params[:action] == 'Submit'
+    update_saved_forms
+    @saved_wsdls += [@params[:wsdl].to_s]
+    save_wsdls
   end
   @wsdl = @params[:wsdl]
   @ast = nil
@@ -50,6 +79,7 @@ post '/build' do
   haml :client
 end
 
+
 post '/update' do
   @client = @params[:client]
   @wsdl = @params[:wsdl]
@@ -58,21 +88,23 @@ post '/update' do
   @endpoint = @params[:endpoint]
   @params = SinatraAppHelpers::update @params
   @input = @params['input']
-  @saved_requests = Dir.entries(Dir.pwd + "/public/saved_forms/requests/").grep(/\.xml$/)
-  @saved_responses = Dir.entries(Dir.pwd + "/public/saved_forms/responses/").grep(/\.xml$/)
-    
+
   if @params[:action] == 'Submit'
     @input = SinatraAppHelpers::send_request(@input,@service_method, @client, @namespace, @wsdl, @endpoint)
     return haml :result
   elsif @params[:action] == 'SaveRequest'
     SaveLoadConvertHelpers::save_request_xml(@input, "public/saved_forms/requests/"+@params["file_name"], @client, @namespace, @wsdl)
+    update_saved_forms
   elsif @params[:action] == 'DeleteRequest'
     SaveLoadConvertHelpers::del_request_xml(@params["file_name"])
+    update_saved_forms
   elsif @params[:action] == 'SaveResponse'
     SaveLoadConvertHelpers::save_request_xml(@input, "public/saved_forms/responses/"+@params["file_name"], @client, @namespace, @wsdl)
+    update_saved_forms
     return haml :result
   elsif @params[:action] == 'DeleteResponse'
     SaveLoadConvertHelpers::del_response_xml(@params["file_name"])
+    update_saved_forms
     return haml :result
   elsif @params[:action] == 'LoadRequest'
     @params['input'] = SaveLoadConvertHelpers::load_request_xml("public/saved_forms/requests/"+@params["file_name"], @client, @namespace, @wsdl)
@@ -80,14 +112,14 @@ post '/update' do
   elsif @params[:action] == 'Upload'
     File.open("public/saved_forms/requests/#{@params['datafile'][:filename]}", "w+").syswrite(@params["datafile"][:tempfile].readlines)
     @params['input'] = SaveLoadConvertHelpers::load_request_xml("public/saved_forms/requests/#{@params['datafile'][:filename]}", @client, @namespace, @wsdl)
+    update_saved_forms
     @input = @params['input']
   elsif @params[:action] == 'Add'
     @input = SinatraAppHelpers::create_element(@input, @params[:element])
   elsif @params[:action] == 'Del'
     @input = SinatraAppHelpers::remove_element(@input, @params[:element])
   end
-    @saved_requests = Dir.entries(Dir.pwd + "/public/saved_forms/requests/").grep(/\.xml$/)
-    @saved_responses = Dir.entries(Dir.pwd + "/public/saved_forms/responses/").grep(/\.xml$/)
+    update_saved_forms
     haml :client
 end
 
